701c25932e2fda18687da23f70d8672d174d3b1b
bruno <pascoal.goncalves.bruno@gmail.com>
2025-01-15 23:56:14 +0100

    dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import ThemeCom from "@/components/ThemeCom";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
@@ -21,14 +22,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header />
-        {children}
-      </body>
-    </html>
+      <html lang="en" suppressHydrationWarning>
+        <body>
+          <ThemeCom>
+            <Header />
+            {children}
+          </ThemeCom>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
